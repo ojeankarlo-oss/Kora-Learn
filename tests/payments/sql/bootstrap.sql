ec784590f93811276274e203c551122b6fbfb1a1
Jean Karlo <ojeankarlo@gmail.com>
2026-09-08 21:54:38 -0300

    ci(payments): make validation permanent

--- tests/payments/sql/bootstrap.sql
+++ tests/payments/sql/bootstrap.sql
@@ -1,7 +1,7 @@
 \set ON_ERROR_STOP on
 
-\connect kora_qa supabase_admin
-grant create on database kora_qa to postgres;
+\connect kora_ci supabase_admin
+grant create on database kora_ci to postgres;
 grant create on schema public to postgres;
 set role supabase_storage_admin;
 create table storage.buckets (id text primary key, name text not null, public boolean not null default false);
@@ -11,4 +11,4 @@ language sql immutable
 as $$ select string_to_array(name, '/') $$;
 grant select, insert, update, delete on storage.buckets, storage.objects to authenticated, service_role;
 reset role;
-\connect kora_qa postgres
+\connect kora_ci postgres
